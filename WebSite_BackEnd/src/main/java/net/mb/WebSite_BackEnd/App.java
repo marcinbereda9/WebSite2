0ad2d488f77c5e3305a7914067a252afa49d0593
mbereda <marcin.bereda@gmail.com>
2019-01-16 18:23:07 +0100

    setup

---
+++ WebSite_BackEnd/src/main/java/net/mb/WebSite_BackEnd/App.java
@@ -0,0 +1,13 @@
+package net.mb.WebSite_BackEnd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
